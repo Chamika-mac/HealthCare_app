437a59b949f5fba5c3a039b6dd7839d9ef593327
Chamika <chamika@fintelex.com>
2020-04-07 14:26:26 +0530

    Itializing Project

---
+++ HealthCare2/src/main/java/com/PAFAssignment/HealthCare2/resources/AdminResource.java
@@ -0,0 +1,17 @@
+package com.PAFAssignment.HealthCare2.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("message")
+public class AdminResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessage() {
+		return "Hello Users!";
+	}
+
+}
