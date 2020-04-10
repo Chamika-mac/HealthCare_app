a86d3ef2cbf03cd4259cac1ed2543610f1945a90
Chamika <chamika@fintelex.com>
2020-04-10 13:00:56 +0530

    Admin services update 01

--- Database/healthcare_db.sql
+++ Database/healthcare_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2020 at 11:27 AM
+-- Generation Time: Apr 10, 2020 at 09:29 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -30,13 +30,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `RECORD_ID` int(10) NOT NULL,
-  `FIRST_NAME` varchar(100) NOT NULL,
-  `LAST_NAME` varchar(100) NOT NULL,
-  `ID_NUMBER` varchar(100) NOT NULL,
-  `ADMIN_ROLE` varchar(100) NOT NULL,
-  `ADMIN_USERNAME` varchar(100) NOT NULL,
-  `ADMIN_PASSWORD` varchar(100) NOT NULL
+  `FIRST_NAME` varchar(100) DEFAULT NULL,
+  `LAST_NAME` varchar(100) DEFAULT NULL,
+  `ID_NUMBER` varchar(100) DEFAULT NULL,
+  `ADMIN_ROLE` varchar(100) DEFAULT NULL,
+  `ADMIN_USERNAME` varchar(100) DEFAULT NULL,
+  `ADMIN_PASSWORD` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`RECORD_ID`, `FIRST_NAME`, `LAST_NAME`, `ID_NUMBER`, `ADMIN_ROLE`, `ADMIN_USERNAME`, `ADMIN_PASSWORD`) VALUES
+(10, 'Chamika', 'Dimantha', '961153301v', 'Administrator', 'chamika', 'chamika123');
 
 -- --------------------------------------------------------
 
@@ -103,7 +110,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `RECORD_ID` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `RECORD_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `doctor`
